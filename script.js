75376958936586bcfaf4f5a82d97036db482d699
DanielPerfeito <dp.ribeiro.1996@gmail.com>
2022-09-18 23:08:26 -0300

    Estrutura de repetição FOR

--- script.js
+++ script.js
@@ -1,50 +1,11 @@
+// Estrutura de repetição FOR.
 
-//Global
+var numero = 10;
 
-var nota01 = 9;
+for(var vez = 0; vez <= numero; vez++){
 
-var nota02 = 9;
+console.log("Executando o FOR pela " + vez + " vez.");
 
-var media = (nota01 + nota02) / 2;
-
-console.log(media);
-
-var conceito = "";
-
-// Estrutura condicional If Else
-
-if (media >= 7) {
-    
-    conceito = "Bom";
 }
 
-else if (media >= 5 && media < 7) {
-    
-    conceito = "Regular";
-}
-
-else {
-
-    conceito = "Ruim";
-}
-
-// Estrutura condicional Switch
-
-switch (conceito) {
-
-    case "Bom": console.log("Você passou com " + media + ", Parabens!");
-        break
-
-    case "Regular":
-        console.log("Você passou com " + media + ", mas pode melhorar.");
-        break
-
-    case "Ruim":
-        console.log("Sua média " + media + " não foi sufissiente.");
-        break
-
-    default: console.log("Houve algum erro!");
-        break
-}
-
-console.log("Conceito: " + conceito);
+console.log("Acabou!");
