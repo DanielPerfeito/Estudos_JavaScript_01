71fb25d8f7482f0a2b15fc29e1ab6072c9b5c8c3
DanielPerfeito <dp.ribeiro.1996@gmail.com>
2022-11-15 15:17:34 -0300

    escopo de variáveis, datas e revisão de objetos

---
+++ objetos_revisao.js
@@ -0,0 +1,17 @@
+
+
+//segunda maneira de criar um objeto
+
+function obj(n, s){
+   
+    return {nome: n, sobrenome:s}
+
+}
+
+var a = obj ("Daniel", "Perfeito")
+var b = obj ("Ana", "Paes")
+var c = obj ("Felipe", "Perfeito")
+
+console.log(a)
+console.log(b)
+console.log(c)
